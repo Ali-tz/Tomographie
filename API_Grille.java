f48c2f768c4eb84bed22645db706d8461e8ac22a
Ali-tz <ali.touzi@gmail.com>
2020-11-21 00:05:40 +0100

    taille des cases de la grille

--- API_Grille.java
+++ API_Grille.java
@@ -3,7 +3,6 @@ import  java.awt.*;
 import Grille.*;
 
 
-
 public class API_Grille{
 	private Grille G;
 	private JFrame frame;
@@ -23,6 +22,7 @@ public class API_Grille{
 		for(int i = 0; i < n; i++){
 			for(int j = 0; j < m; j++){  				  /* Mise en place des case de la grille */
 				cases[i][j] = new JPanel();
+				cases[i][j].setSize(frame.getWidth()/n,frame.getHeight()/m);
 				if(this.G.getCouleur(i, j) == 2){
 					cases[i][j].setBackground(Color.BLACK);
 				}else if(this.G.getCouleur(i, j) == 1){
