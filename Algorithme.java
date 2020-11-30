83a6e84380630a13275359f45965b2d830e9dd32
Ali-tz <ali.touzi@gmail.com>
2020-11-30 14:45:30 +0100

    mise au propre

--- Algorithme.java
+++ Algorithme.java
@@ -16,6 +16,32 @@ public class Algorithme{
      * 
      * /
 
+     /*public boolean T(int i, int j, int l){
+
+        if (l==0){
+            return false;
+        }
+
+        int sl = this.G.getSequencesLigne()[i].getSequence_i(l-1);
+
+        if(l == 0){
+            return true;
+        }
+
+        else{
+            if(j < sl - 1){
+                return false;
+            }else if(j == sl - 1){
+                if(l == 1){
+                    return true;
+                }
+                return false;
+
+            }else{
+                return T(i,j - sl - 1, l-1) || T(i,j-1, l);
+            }
+        }
+    } */
    
 
         /*------------------ GENERALISATION ------------------------------------*/
