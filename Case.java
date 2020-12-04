6bcd78a8ab2fdb6733e400104927b83393a1633f
Ali-tz <ali.touzi@gmail.com>
2020-12-04 23:47:00 +0100

    class Case commenté

--- Case.java
+++ Case.java
@@ -1,9 +1,79 @@
+/**
+ * Case is the class representing a black, white or none colored case.
+ * Start class for this poject.
+ * 
+ * @author Sylvain Rakotomalala, Ali Touzi
+ */
 public class Case{
 
-    private int x,y;
-    private int couleur; /* 0 non initialisé --- 1 = BLANC ----- 2 = BLACKNOIR*/
+
+
+    //  ************************************************************************
+
+    //  Fields
+
+    //  ************************************************************************
+
+    /**
+     * private field, represents the x-axis
+     * 
+     * @see Case#Case(int, int, int)
+     * @see Case#Case(int, int)
+     * @see Case#getX()
+     */
+    private int x;
+
+    /**
+     * private field, represents the y-axis
+     * 
+     * @see Case#Case(int, int, int)
+     * @see Case#Case(int, int)
+     * @see Case#getY()
+     */
+    private int y;
+
+
+    /**
+     * private field, represents the color of this {@link #Case}; 0 means no color, 1 means white and 2 means black
+     * 
+     * @see Case#Case(int, int, int)
+     * @see Case#Case(int, int)
+     * @see Case#getCouleur()
+     * @see Case#changeCouleur(int)
+     */
+    private int couleur; 
+
+    /**
+     * private field, tells if this {@link #Case} was recently visited
+     * 
+     * @see Case#getRecent()
+     * @see Case#setRecent(boolean)
+     */
     private boolean recent;
 
+
+
+
+    //  *************************************************************************
+
+    //  Constructors
+
+    //  *************************************************************************
+
+    /**
+     * Parameterized constructor.
+     * 
+     * The recent value is always set to false.
+     * 
+     * @param x The coordinate value  that will be set on the x-axis to this {@link #Case}.
+     * @param y The coordinate value that will be set on the y-axis to this {@link #Case}. 
+     * @param couleur The color value that will be set on couleur to this {@link #Case}.
+     * 
+     * @see Case#x
+     * @see Case#y
+     * @see Case#couleur
+     * 
+     */
     public Case(int x, int y, int couleur){
         this.x = x;
         this.y = y;
@@ -11,33 +81,87 @@ public class Case{
         this.recent = false;
     }
 
+    /**
+     * Default constructor.
+     * 
+     * The color value will be 0 and the recent value is always set to false..
+     * 
+     * @param x The coordinate value  that will be set on the x-axis to this {@link #Case}.
+     * @param y he coordinate value that will be set on the y-axis to this {@link #Case}. 
+     * 
+     * @see Case#x
+     * @see Case#y
+     */
     public Case(int x, int y){
         this(x, y, 0);
     }
 
-    /*public Case(int y){
-        this.y = y;
-        this.couleur = true;
-    }*/
-
+    
+    /**
+     * This method changes the color of the case
+     * 
+     * @param cl Value of the new color
+     * 
+     * @see Case#couleur
+     * 
+     */
     public void changeCouleur(int cl){
         couleur = cl;
     }
+    
+
+    /**
+     * This method sets the field {@link #recent} to bool value in this {@link #Case}
+     * 
+     * @param bool new value for {@link #recent} in this {@link #Case}
+     * 
+     * @see Case#recent
+     */
     public void setRecent(boolean bool){
         recent = bool;
     }
 
+
+    /**
+     * This method is used to reach the value of {@link #recent} in this {@link #Case}
+     * 
+     * @return The value of {@link #recent} in this {@link #Case}
+     * 
+     * @see Case#recent
+     */
     public boolean getRecent(){
         return recent;
     }
+
+    /**
+     * This method returns the value of {@link #x} of this {@link #Case}
+     * 
+     * @return The value of {@link #x} of this {@link #Case}
+     * 
+     * @see Case#x
+     */
     public int getX(){
         return x;
     }
 
+    /**
+     * This method returns the value of {@link #y} of this {@link #Case}
+     * 
+     * @return The value of {@link #x} of this {@link #Case}
+     *
+     * @see Case#y
+     * /
     public int getY(){
         return y;
     }
 
+    /**
+     * This method returns the value of {@link #couleur} of this {@link #Case}
+     * 
+     * @return The value of {@link #couleur} of this {@link #Case}
+     * 
+     * @see Case#couleur
+     */
     public int getCouleur(){
         return couleur;
     }
