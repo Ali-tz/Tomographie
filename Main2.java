bef73af96c6a51459ec1ccd4b84bd2a97bdb6eec
rassyl2 <srakotomalala94@gmail.com>
2020-12-07 17:16:00 +0100

    soutenance

---
+++ Main2.java
@@ -0,0 +1,74 @@
+import java.nio.file.*;
+import java.time.*;
+import java.io.*;
+
+
+
+
+/**
+ * Main class
+ * 
+ * @author Sylvain Rakotomalala, Ali Touzi
+ */
+public class Main2{
+
+    /**
+     * This method is the Main function. 
+     * It parses the file on argument and uses it to crate the {@link #Grille}, then solves it and display it on screen.
+     * 
+     * @param args Takes a file in instances as an argument.
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        if(args.length != 1){
+            System.out.println("Erreur: <fichier.txt>");
+        }
+        
+        String str = new String(Files.readAllBytes(Paths.get(args[0])));
+
+        //System.out.println(grille);
+        
+        Grille grille = new Grille(str);
+
+        /*long timeElapsed = 0;*/
+    
+        
+        /* Q10
+        for(int i=0;i<10;i++){
+            long start = System.nanoTime();
+  
+            Algorithme test = new Algorithme(grille);
+            Grille grilleColo = test.coloration();
+            long finish = System.nanoTime();
+            System.out.println(finish - start);
+            timeElapsed += finish - start;
+        
+        } 
+        
+      
+        System.out.println(timeElapsed/11);*/
+
+        
+        /*for(int i=0;i<10;i++){
+            long start = System.nanoTime();
+  
+            Grille grilleColo = grille.enumeration();
+            long finish = System.nanoTime();
+            System.out.println(finish - start);
+            timeElapsed += finish - start;
+        
+        } */
+        
+      
+        /*System.out.println(timeElapsed/11);*/
+        Algorithme test = new Algorithme(grille);
+        Grille grilleColo = test.coloration();
+
+        //System.out.println(grilleColo);
+
+        API_Grille f = new API_Grille(grilleColo);
+        f.run();
+
+    }
+  
+}
